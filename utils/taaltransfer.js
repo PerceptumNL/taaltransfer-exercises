825adef82596f699f6d0e6a17ef220d81bca669a
Elise van der Pol <koster.elise@gmail.com>
2013-10-28 18:11:29 +0100

    Can now display sentences individually

--- utils/taaltransfer.js
+++ utils/taaltransfer.js
@@ -1,19 +1,32 @@
-// Example usage:
-// <var>person(1)</var> traveled 5 mi by <var>vehicle(1)</var>. Let
-// <var>his(1)</var> average speed be <var>personVar(1)</var>.
-// Let <var>person(2)</var>'s speed be <var>personVar(2)</var>.
-//
-// Note that initials (-Var) are guaranteed to be unique in each category,
-// but not across them.
-
 $.extend(KhanUtil, {
 
+  /***
+    Return array of sentence:category pairs
+    The Ajax call will save the response text in var words, which is then split at
+    newlines to separate the sentences.
+    Type: Array of strings
+  ***/
+  
   readFile: function(file){
     console.log("Reading " + file + "...");
     var words = $.ajax({type: "GET", url: file, async: false}).responseText;
-    console.log(words);
-    console.log(words.responseText);
-    return words;
+    var wordArray = words.split(/\n/);
+    return wordArray;
+  },
+  
+  /***
+    Return an array that contains two elements: a sentence and a category.
+    The sentence:category strings are split at the occurrence of a dot followed by a comma
+     (to prevent accidental splitting at grammatical commas), stripped of trailing 
+     whitespace and then given a period.
+    Type: Array of strings
+  ***/
+  
+  fixSentence: function(sentence){
+    var pair = sentence.split(/.,/);
+    pair[0] = pair[0].replace(/(^\s*)|(\s*$)/gi,"");
+    pair[0] = pair[0].concat(".");
+    return pair;
   }
 });
 
