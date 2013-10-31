cc95d27964fa05362340e1a57a8a05d515185a2b
Elise van der Pol <koster.elise@gmail.com>
2013-10-31 12:33:05 +0100

    now working on solutionbox etc

--- utils/taaltransfer.js
+++ utils/taaltransfer.js
@@ -30,12 +30,57 @@ $.extend(KhanUtil, {
   },
   
   /***
+    Return an array of tuples containing the grammatical name of every sentence part.
+    The sentence is split at the comma, dividing all parts and names into different 
+    elements. Each sentence:name pair is turned into an array and added 
+    to the main array. 
+    Type: Array of arrays
   ***/
+  
   fixParts: function(sentence){
     var parts = sentence.split(",");
-    for(var i=0; i<parts.length;i=i+2){
-      console.log(parts[i] + " is " + parts[i+1]);
+    var everything = [];
+    var length = parts.length/2;
+    var tmp = [];
+    for(var i=0; i<length;i=i+2){
+      tmp = [];    
+      tmp.push(parts[i]);
+      tmp.push(parts[i+1]);
+      everything.push(tmp);
+   
     }
+    return everything;
+  },
+  
+  /***
+    Return the line containing the last sentence in a certain category.
+    Every sentence is split using fixSentence, then its category is checked against the 
+    category following the one we want. If it is higher than that category, the line 
+    number of the current sentence gets returned.
+    Type: number
+  ***/
+  
+  lastCatSentence: function(category, wordArr){
+    var length = wordArr.length;
+    var tmp = 0;
+    for(var i=0; i<length; i++){
+      tmp = this.fixSentence(wordArr[i]);
+      if(tmp[1] == (category+1)){
+        return (i);
+      }
+    }
+    return i;
+  },
+  
+  /***
+    Return the value that belongs to the requested grammatical name, for example "pv".
+    This function checks all uneven indexes to see if the name is there, and if so,
+    returns the element before it.
+    Type: string
+  ***/
+  findNameValue: function(sentence, name){
+    console.log(sentence);
+    console.log(name);
   }
 });
 
