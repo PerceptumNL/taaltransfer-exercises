ea74b9758c7d2027add8009711fffa174128358c
Sergio <sergio@perceptum.nl>
2014-01-14 14:57:41 +0100

    Add colors.js for splitting backgrounds

---
+++ colors.js
@@ -0,0 +1,20 @@
+
+
+var colors = {
+  //"blue": "#0074d9",
+  "aqua": "#7fdbff",
+  "teal": "#39cccc",
+  "olive": "#3d9970",
+  "green": "#2ecc40",
+  "lime": "#01ff70",
+  "yellow": "#ffdc00",
+  "orange": "#ff851b",
+  "red": "#ff4136",
+  "fuchsia": "#ff4136",
+  "purple": "#b10dc9",
+  "maroon": "#85144b",
+  "white": "#ffffff",
+  "gray":"#aaaaaa",
+  "silver": "#dddddd",
+}
+
